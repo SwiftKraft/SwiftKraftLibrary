07f98b5a81ea5088299bc9a35825d803d7386ba9
SwiftKraft <shihuamian@gmail.com>
2024-11-05 14:10:39 -0500

    asdasd

--- Assets/SwiftKraft/Gameplay/Weapons/WeaponAmmo.cs
+++ Assets/SwiftKraft/Gameplay/Weapons/WeaponAmmo.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace SwiftKraft.Gameplay.Weapons
 {
     public abstract class WeaponAmmo : WeaponBase
@@ -5,13 +7,25 @@ namespace SwiftKraft.Gameplay.Weapons
         public int MaxAmmo { get; set; }
         public int CurrentAmmo { get; set; }
 
-        public bool TryUseAmmo(int ammo = 1)
+        public bool HasAmmo(int ammo = 1) => CurrentAmmo >= ammo;
+
+        public bool TryUseAmmo(int ammo, out int overused)
         {
-            if (CurrentAmmo <= 0)
+            if (!HasAmmo())
+            {
+                overused = 0;
                 return false;
+            }
 
             CurrentAmmo -= ammo;
+            overused = CurrentAmmo >= 0 ? 0 : Mathf.Abs(CurrentAmmo);
+
+            if (CurrentAmmo < 0)
+                CurrentAmmo = 0;
+
             return true;
         }
+
+        public bool TryUseAmmo(int ammo = 1) => TryUseAmmo(ammo, out _);
     }
 }
--- Assets/SwiftKraft/Utility/Extensions/QuaternionExtensions.cs
+++ Assets/SwiftKraft/Utility/Extensions/QuaternionExtensions.cs
@@ -11,12 +11,15 @@ namespace SwiftKraft.Utils
 
             Vector3 c = current.eulerAngles;
             Vector3 t = target.eulerAngles;
+
             return Quaternion.Euler(
               Mathf.SmoothDampAngle(c.x, t.x, ref currentVelocity.x, smoothTime),
               Mathf.SmoothDampAngle(c.y, t.y, ref currentVelocity.y, smoothTime),
               Mathf.SmoothDampAngle(c.z, t.z, ref currentVelocity.z, smoothTime)
             );
         }
+
+
 
         public static float NormalizeAngle(this float a) => a > 180f ? a - 360f : a;
 
