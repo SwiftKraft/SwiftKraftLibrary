1eda868a06261f957007dc623c361eda5c5a2c5b
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2024-11-24 09:30:45 -0500

    item stuff

--- Assets/SwiftKraft/Gameplay/Inventory/Items/EquippedItem.cs
+++ Assets/SwiftKraft/Gameplay/Inventory/Items/EquippedItem.cs
@@ -1,11 +1,34 @@
+using SwiftKraft.Utils;
+using System;
 using UnityEngine;
 
 namespace SwiftKraft.Gameplay.Inventory.Items
 {
     public class EquippedItem : MonoBehaviour
     {
+        public ItemEquipper Parent { get; private set; }
+
         public EquippableItemType Item { get; private set; }
 
-        public void Init(EquippableItemType type) => Item = type;
+        public void Init(EquippableItemType type, ItemEquipper parent)
+        {
+            Parent = parent;
+            Item = type;
+        }
+
+        public event Action OnFailedUnequip;
+
+        public readonly BooleanLock CanUnequip = new();
+
+        public void Unequip()
+        {
+            if (!CanUnequip)
+            {
+                OnFailedUnequip?.Invoke();
+                return;
+            }
+
+            Parent.Equip();
+        }
     }
 }
--- Assets/SwiftKraft/Gameplay/Inventory/Items/ItemEquipper.cs
+++ Assets/SwiftKraft/Gameplay/Inventory/Items/ItemEquipper.cs
@@ -1,9 +1,101 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace SwiftKraft.Gameplay.Inventory.Items
 {
     public class ItemEquipper : MonoBehaviour
     {
+        public readonly List<EquippedItem> EquippedItemCache = new();
 
+        public Transform Workspace;
+
+        public event Action<EquippedItem> OnEquip;
+
+        public EquippedItem Current { get; private set; }
+        public EquippableItemType WishEquip
+        {
+            get => _wishEquip;
+            set
+            {
+                if (_wishEquip == value)
+                    return;
+
+                _wishEquip = value;
+
+                if (!unequipping)
+                {
+                    if (Current == null)
+                        Equip();
+                    else
+                        Unequip();
+                }
+            }
+        }
+        EquippableItemType _wishEquip;
+
+        bool unequipping;
+
+        private void Awake()
+        {
+            EquippedItemCache.AddRange(GetComponentsInChildren<EquippedItem>());
+
+            ResetAll();
+        }
+
+        public bool HasEquippedItem(EquippableItemType type, out EquippedItem it)
+        {
+            foreach (EquippedItem item in EquippedItemCache)
+                if (item.Item == type)
+                {
+                    it = item;
+                    return true;
+                }
+            it = null;
+            return false;
+        }
+
+        public bool AddEquippedItem(GameObject prefab, out EquippedItem it)
+        {
+            if (!prefab.TryGetComponent(out EquippedItem item))
+            {
+                it = null;
+                return false;
+            }
+            it = Instantiate(item, Workspace);
+            EquippedItemCache.Add(it);
+            return true;
+        }
+
+        public void ResetAll()
+        {
+            foreach (EquippedItem item in EquippedItemCache)
+                item.gameObject.SetActive(false);
+            unequipping = false;
+        }
+
+        public void Unequip()
+        {
+            if (Current != null)
+            {
+                Current.Unequip();
+                unequipping = true;
+            }
+        }
+
+        public void Equip()
+        {
+            EquippedItem eq = null;
+            if (Current != null)
+                Current.gameObject.SetActive(false);
+            else if (WishEquip != null && (HasEquippedItem(WishEquip, out EquippedItem it) || AddEquippedItem(WishEquip.EquippedPrefab, out it)))
+                eq = it;
+
+            if (eq != null)
+                OnEquip?.Invoke(eq);
+
+            Current = eq;
+            unequipping = false;
+        }
     }
 }
