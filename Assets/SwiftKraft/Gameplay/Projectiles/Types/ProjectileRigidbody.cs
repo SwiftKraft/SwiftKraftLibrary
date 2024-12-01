fdcd272abf051dc2d7faa9b8d0131389da8962bd
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2024-12-01 10:28:01 -0500

    Update ProjectileRigidbody.cs

--- Assets/SwiftKraft/Gameplay/Projectiles/Types/ProjectileRigidbody.cs
+++ Assets/SwiftKraft/Gameplay/Projectiles/Types/ProjectileRigidbody.cs
@@ -65,16 +65,22 @@ namespace SwiftKraft.Gameplay.Projectiles
             Vector3 normal = collision.GetContact(0).normal.normalized;
             Vector3 direction = Vector3.Reflect(prevVel.normalized, normal);
 
-             Debug.DrawRay(collision.GetContact(0).point, prevVel.normalized, Color.green, 5f);
-             Debug.DrawRay(collision.GetContact(0).point, normal, Color.blue, 5f);
-             Debug.DrawRay(collision.GetContact(0).point, direction, Color.red, 5f);
-
+#if UNITY_EDITOR
+            Debug.DrawRay(collision.GetContact(0).point, prevVel.normalized, Color.green, 5f);
+            Debug.DrawRay(collision.GetContact(0).point, normal, Color.blue, 5f);
+            Debug.DrawRay(collision.GetContact(0).point, direction, Color.red, 5f);
+#endif
             Rigidbody.angularVelocity = Vector3.zero;
             transform.forward = direction.normalized;
             Rigidbody.velocity = Quaternion.LookRotation(direction) * Speed;
         }
 
-        public virtual void EndHit(Collision collision) => Destroy(gameObject);
+        public virtual void EndHit(Collision collision)
+        {
+            Hit(collision);
+            Destroy(gameObject);
+        }
+
         public virtual void Hit(Collision collision) { }
     }
 }
