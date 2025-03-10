e7ed8a4660031f90e1fb53bd697b1b54b25ec039
SwiftKraft <shihuamian@gmail.com>
2025-03-10 09:38:02 -0400

    a

--- Assets/SwiftKraft/Gameplay/Projectiles/Components/ProjectileHitObject.cs
+++ Assets/SwiftKraft/Gameplay/Projectiles/Components/ProjectileHitObject.cs
@@ -8,11 +8,13 @@ namespace SwiftKraft.Gameplay.Projectiles
     {
         public Spawner[] Spawners;
 
+        public bool InHierarchy = true;
+
         public override void Init() => Component.OnHit += OnHit;
 
         protected virtual void OnDestroy() => Component.OnHit -= OnHit;
 
-        protected virtual void OnHit(ProjectileBase.HitInfo info) => Spawners.GetWeightedRandom().Spawn(info.Position, Quaternion.LookRotation(info.Normal, Vector3.up));
+        protected virtual void OnHit(ProjectileBase.HitInfo info) => Spawners.GetWeightedRandom().Spawn(info.Position, Quaternion.LookRotation(info.Normal, Vector3.up), InHierarchy, info.Object.transform);
 
         [Serializable]
         public class Spawner : IWeight
@@ -21,10 +23,14 @@ namespace SwiftKraft.Gameplay.Projectiles
             public int Weight { get; private set; }
             public GameObject[] Spawnees;
 
-            public void Spawn(Vector3 pos, Quaternion rot)
+            public void Spawn(Vector3 pos, Quaternion rot, bool inHierarchy, Transform hit)
             {
                 foreach (GameObject go in Spawnees)
-                    Instantiate(go, pos, rot);
+                {
+                    GameObject spawn = Instantiate(go, pos, rot);
+                    if (inHierarchy)
+                        spawn.transform.parent = hit;
+                }
             }
         }
     }
