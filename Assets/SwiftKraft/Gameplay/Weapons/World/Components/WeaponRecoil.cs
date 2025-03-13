01aa0ff052d7da968a63d05c2187e33cc6801e6b
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-03-13 17:44:08 -0400

    a

--- Assets/SwiftKraft/Gameplay/Weapons/World/Components/WeaponRecoil.cs
+++ Assets/SwiftKraft/Gameplay/Weapons/World/Components/WeaponRecoil.cs
@@ -17,37 +17,66 @@ namespace SwiftKraft.Gameplay.Weapons
         public AnimationCurve DecayRate;
         public Accumulator Heat = new(Mathf.Infinity);
 
+        public Vector3 Position
+        {
+            get => modifier == null ? GetRecoilTransform().localPosition : modifier.Position;
+            protected set
+            {
+                if (modifier != null)
+                    modifier.Position = value;
+                else
+                    GetRecoilTransform().localPosition = value;
+            }
+        }
+
+        public Quaternion Rotation
+        {
+            get => modifier == null ? GetRecoilTransform().localRotation : modifier.Rotation;
+            protected set
+            {
+                if (modifier != null)
+                    modifier.Rotation = value;
+                else
+                    GetRecoilTransform().localRotation = value;
+            }
+        }
+
         public bool Smooth = false;
+
+        MultiModifyTransform.Modifier modifier;
 
         protected virtual void Awake()
         {
             RecoilTransform = transform.root.GetComponentInChildren<IRecoilTransform>();
             Component.OnAttack += OnAttack;
+
+            if (GetRecoilTransform().TryGetComponent(out MultiModifyTransform tr))
+                modifier = tr.AddModifier();
         }
 
-        protected virtual void OnDestroy() => Component.OnAttack -= OnAttack;
+        protected virtual void OnDestroy()
+        {
+            Component.OnAttack -= OnAttack;
+            modifier?.Remove();
+        }
 
         protected virtual void FixedUpdate()
         {
             Heat.CanDecay = !Component.Attacking;
             Heat.Tick(DecayRate.Evaluate(Heat.CurrentValue) * DecayMultiplier * Time.fixedDeltaTime);
-            Transform tr = GetRecoilTransform();
-            if (tr != null)
-            {
-                if (Smooth && !Heat.CanDecay)
-                    ApplyRecoil(tr);
-                DecayRecoil(tr);
-            }
+            if (Smooth && !Heat.CanDecay)
+                ApplyRecoil();
+            DecayRecoil();
         }
 
-        protected abstract void DecayRecoil(Transform trans);
-        protected abstract void ApplyRecoil(Transform trans);
+        protected abstract void DecayRecoil();
+        protected abstract void ApplyRecoil();
 
         protected virtual void OnAttack(GameObject go)
         {
             Heat.Increment(1f);
             if (!Smooth)
-                ApplyRecoil(GetRecoilTransform());
+                ApplyRecoil();
         }
     }
 }
--- Assets/SwiftKraft/Gameplay/Weapons/World/Components/WeaponRecoil3D.cs
+++ Assets/SwiftKraft/Gameplay/Weapons/World/Components/WeaponRecoil3D.cs
@@ -14,12 +14,16 @@ namespace SwiftKraft.Gameplay.Weapons
 
         Vector3 curVel;
 
-        protected override void DecayRecoil(Transform recoil) => recoil.localRotation = recoil.localRotation.SmoothDamp(Quaternion.Euler(Vector3.zero), ref curVel, 1f / (DecayRate.Evaluate(Heat.CurrentValue) * DecayMultiplier * RecoverMultiplier));
+        protected override void DecayRecoil()
+        {
+            Rotation = Rotation.SmoothDamp(Quaternion.Euler(Vector3.zero), ref curVel, 1f / (DecayRate.Evaluate(Heat.CurrentValue) * DecayMultiplier * RecoverMultiplier));
+            Debug.Log(Rotation, GetRecoilTransform());
+        }
 
-        protected override void ApplyRecoil(Transform recoil)
+        protected override void ApplyRecoil()
         {
             Vector2 move = new Vector2(-Vertical.Evaluate(Heat.CurrentValue) * VerticalMultiplier, Horizontal.Evaluate(Heat.CurrentValue) * HorizontalMultiplier) * RecoilMultiplier;
-            recoil.Rotate(move * (!Smooth ? 1f : Time.fixedDeltaTime), Space.Self);
+            Rotation *= Quaternion.Euler(move * (!Smooth ? 1f : Time.fixedDeltaTime));
         }
     }
 }
