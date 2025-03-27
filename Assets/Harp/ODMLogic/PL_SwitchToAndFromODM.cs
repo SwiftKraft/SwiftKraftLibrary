1bf64640f12b899de28ca5ff7e969f2094a82d71
HarpDev <hharperSu@proton.me>
2025-03-26 21:19:30 -0600

    a

---
+++ Assets/Harp/ODMLogic/PL_SwitchToAndFromODM.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SwiftKraft.Gameplay.Inventory;
+using SwiftKraft.Gameplay.Inventory.Items;
+using SwiftKraft.Gameplay.Common.FPS;
+
+public class PL_SwitchToAndFromODM : MonoBehaviour
+{
+    // Start is called before the first frame update\
+    [SerializeField]
+    private SimpleFPSInventory FPS_Items;
+    [SerializeField]
+    PL_ODM FPS_ODM;
+
+        public int switchTimeInSeconds;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
