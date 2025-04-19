ef4b360e45eda28a85f50f310e11dbc816357d4f
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-04-18 20:00:13 -0400

    a

--- Assets/SwiftKraft/Gameplay/NPCs/NPCAttacker.cs
+++ Assets/SwiftKraft/Gameplay/NPCs/NPCAttacker.cs
@@ -4,21 +4,20 @@ namespace SwiftKraft.Gameplay.NPCs
 {
     public abstract class NPCAttacker : NPCModuleBase
     {
-        public WeaponBase Weapon;
-
         protected NPCScannerBase.Package ScannerData;
 
-        public bool HasTarget => (ScannerData != null || Parent.Values.TryGet(NPCScannerBase.DataID, out ScannerData)) && ScannerData.Targets.Count >= 0;
+        public virtual bool HasTarget => (ScannerData != null || Parent.Values.TryGet(NPCScannerBase.DataID, out ScannerData)) && ScannerData.Targets.Count >= 0;
 
-        protected override void Awake()
-        {
-            base.Awake();
-            if (Weapon == null)
-                Weapon = GetComponentInChildren<WeaponBase>();
-        }
+        public abstract bool CanAttack { get; }
 
         protected virtual void Start() => ScannerData = Parent.Values.Get<NPCScannerBase.Package>(NPCScannerBase.DataID);
 
-        public virtual void Attack() => Weapon.Attack();
+        protected virtual void FixedUpdate()
+        {
+            if (HasTarget && CanAttack)
+                Attack();
+        }
+
+        public abstract void Attack();
     }
 }
