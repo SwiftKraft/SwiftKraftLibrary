a737dbcb29d9d4b24d934584c26afec3808f030c
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-05-03 08:14:44 -0400

    a

--- Assets/SwiftKraft/Utility/Components/RandomAudio.cs
+++ Assets/SwiftKraft/Utility/Components/RandomAudio.cs
@@ -10,6 +10,7 @@ namespace SwiftKraft.Utils
         public AudioClip[] Clips;
 
         public bool PlayOnAwake;
+        public bool Override;
 
         public float Chance = 1f;
 
@@ -28,7 +29,11 @@ namespace SwiftKraft.Utils
                 chanceOverride = Chance;
 
             if (chanceOverride == 1f || Random.Range(0f, 1f) <= chanceOverride)
+            {
+                if (Override)
+                    Audio.Stop();
                 Audio.PlayOneShot(Clips.GetRandom(ref lastRandom));
+            }
             else
                 Audio.Stop();
         }
