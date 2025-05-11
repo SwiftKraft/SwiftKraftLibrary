d1fdb4c0cc272f5a27775ce565b6c8294cb74ed6
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-05-11 15:11:02 -0400

    Update ItemManager.cs

--- Assets/SwiftKraft/Gameplay/Inventory/Items/Data/ItemManager.cs
+++ Assets/SwiftKraft/Gameplay/Inventory/Items/Data/ItemManager.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+using SwiftKraft.Saving.Progress;
 using System;
 using System.Collections.Generic;
 
@@ -6,7 +8,8 @@ namespace SwiftKraft.Gameplay.Inventory.Items
     public static class ItemManager
     {
         public static readonly Dictionary<string, ItemType> Registered = new();
-        public static readonly Dictionary<Guid, ItemInstance> Instances = new();
+        public static Dictionary<Guid, ItemInstance> Instances => ItemScene.Instances;
+        public static Data ItemScene = new();
 
         public static bool Register(this ItemType type) => Registered.TryAdd(type.ID, type);
 
@@ -32,5 +35,11 @@ namespace SwiftKraft.Gameplay.Inventory.Items
         }
 
         public static ItemInstance GetInstance(Guid id) => Instances.ContainsKey(id) ? Instances[id] : null;
+
+        public class Data : Progressable
+        {
+            [JsonProperty]
+            public Dictionary<Guid, ItemInstance> Instances = new();
+        }
     }
 }
