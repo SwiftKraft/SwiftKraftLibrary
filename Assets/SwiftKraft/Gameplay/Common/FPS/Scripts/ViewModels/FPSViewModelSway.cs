dba37ea9b78e3b18e9e2aa8af119a8ac7ccc00d5
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-05-24 21:37:16 -0400

    a

--- Assets/SwiftKraft/Gameplay/Common/FPS/Scripts/ViewModels/FPSViewModelSway.cs
+++ Assets/SwiftKraft/Gameplay/Common/FPS/Scripts/ViewModels/FPSViewModelSway.cs
@@ -7,8 +7,8 @@ namespace SwiftKraft.Gameplay.Common.FPS.ViewModels
     {
         public Vector2 InputAxis => new(Mathf.Clamp(-Input.GetAxisRaw("Mouse Y"), -Maximum, Maximum), Mathf.Clamp(Input.GetAxisRaw("Mouse X"), -Maximum, Maximum));
 
-        public float Multiplier = 2f;
-        public float TiltMultiplier = 1f;
+        public ModifiableStatistic Multiplier = new(1f);
+        public ModifiableStatistic TiltMultiplier = new(-2f);
         public float Maximum = 3f;
         public float TiltMaximum = 3f;
         public float SmoothTime = 0.2f;
