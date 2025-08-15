40602c3cc76cc9289c6ddccf0126ba4867d9f4ee
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-08-15 13:46:42 -0400

    stuff

---
+++ Assets/MoonHaunted/Scripts/AI/Map/PointOfInterest.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointOfInterest : MonoBehaviour
+{
+    public static readonly HashSet<PointOfInterest> AllPoints = new();
+    public PointFlags Flags;
+
+    private void Awake() => AllPoints.Add(this);
+
+    private void OnDestroy() => AllPoints.Remove(this);
+}
+
+[Flags]
+public enum PointFlags : byte
+{
+    None = 0,
+    FlankSecurity = 1 << 0,
+    Defensive = 1 << 1,
+    Offensive = 1 << 2,
+    HidingSpot = 1 << 3,
+}
