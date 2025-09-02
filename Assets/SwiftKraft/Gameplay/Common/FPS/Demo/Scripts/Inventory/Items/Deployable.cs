c7dc58420bcf3ad9120bfed49226adb4ca2c0857
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-09-01 20:57:28 -0400

    asd

---
+++ Assets/SwiftKraft/Gameplay/Common/FPS/Demo/Scripts/Inventory/Items/Deployable.cs
@@ -0,0 +1,12 @@
+using SwiftKraft.Gameplay.Inventory.Items;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwiftKraft.Gameplay.Common.FPS.Demo
+{
+    public class Deployable : EquippedItemBase
+    {
+
+    }
+}
