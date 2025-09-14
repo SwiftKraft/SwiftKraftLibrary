587c390353d423f6a9fe0e8f6978a7d50cce0e51
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-09-14 10:57:26 -0400

    stuff

--- Assets/SwiftKraft/Gameplay/Animation/AnimatorMovementSetter.cs
+++ Assets/SwiftKraft/Gameplay/Animation/AnimatorMovementSetter.cs
@@ -1,6 +1,6 @@
 using SwiftKraft.Gameplay.Motors;
 using SwiftKraft.Utils;
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,16 +10,40 @@ namespace SwiftKraft.Gameplay.Animation
     {
         public new SmoothDampInterpolater Interpolater;
 
+        [Header("Speed Changes")]
+        public string SpeedParameterName = "MovementMultiplier";
+        public bool Raw;
+        public float Multiplier = 1f;
+        public StateMultiplier[] StateMultipliers;
+
+        private readonly Dictionary<int, float> multipliers = new();
+
         public MotorBase Motor { get; private set; }
 
         protected override void Awake()
         {
             base.Awake();
             Motor = GetComponentInParent<MotorBase>();
+            foreach (StateMultiplier state in StateMultipliers)
+                multipliers.Add(state.State, state.Multiplier);
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            Animator.SetFloatSafe(SpeedParameterName, (Raw ? Motor.RawMoveFactorRate : Motor.MoveFactorRate) * (!multipliers.ContainsKey(Motor.State) ? 1f : multipliers[Motor.State]) * Multiplier);
         }
 
         public override Interpolater AssignInterpolater() => Interpolater;
 
         public override float GetMaxValue() => Motor.State;
+
+        [Serializable]
+        public class StateMultiplier
+        {
+            public int State;
+            public float Multiplier;
+        }
     }
 }
