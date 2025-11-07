2d5135cb9687c980c79284d59a96790282366804
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-11-07 10:41:13 -0500

    a

--- Assets/SwiftKraft/Utility/Extensions/AnimatorExtensions.cs
+++ Assets/SwiftKraft/Utility/Extensions/AnimatorExtensions.cs
@@ -6,6 +6,38 @@ namespace SwiftKraft.Utils
 {
     public static class AnimatorExtensions
     {
+        public static bool HasParameter(this Animator animator, string paramName)
+        {
+            foreach (AnimatorControllerParameter param in animator.parameters)
+                if (param.name == paramName)
+                    return true;
+            return false;
+        }
 
+        public static void SetFloatSafe(this Animator animator, string paramName, float value)
+        {
+            if (animator.HasParameter(paramName))
+                animator.SetFloat(paramName, value);
+        }
+
+        public static void SetIntegerSafe(this Animator animator, string paramName, int value)
+        {
+            if (animator.HasParameter(paramName))
+                animator.SetInteger(paramName, value);
+        }
+
+        public static void SetBoolSafe(this Animator animator, string paramName, bool value)
+        {
+            if (animator.HasParameter(paramName))
+                animator.SetBool(paramName, value);
+        }
+
+        public static bool CheckName(this AnimatorStateInfo info, ICollection<string> stateNames)
+        {
+            foreach (string str in stateNames)
+                if (info.IsName(str))
+                    return true;
+            return false;
+        }
     }
 }
--- Assets/SwiftKraft/Utility/Extensions/MiscExtensions.cs
+++ Assets/SwiftKraft/Utility/Extensions/MiscExtensions.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace SwiftKraft.Utils
@@ -32,40 +31,6 @@ namespace SwiftKraft.Utils
 
         public static bool InLayerMask(this GameObject obj, LayerMask mask) => obj.layer.InLayerMask(mask);
         public static bool InLayerMask(this int layer, LayerMask mask) => (mask.value & (1 << layer)) != 0;
-
-        public static bool HasParameter(this Animator animator, string paramName)
-        {
-            foreach (AnimatorControllerParameter param in animator.parameters)
-                if (param.name == paramName)
-                    return true;
-            return false;
-        }
-
-        public static void SetFloatSafe(this Animator animator, string paramName, float value)
-        {
-            if (animator.HasParameter(paramName))
-                animator.SetFloat(paramName, value);
-        }
-
-        public static void SetIntegerSafe(this Animator animator, string paramName, int value)
-        {
-            if (animator.HasParameter(paramName))
-                animator.SetInteger(paramName, value);
-        }
-
-        public static void SetBoolSafe(this Animator animator, string paramName, bool value)
-        {
-            if (animator.HasParameter(paramName))
-                animator.SetBool(paramName, value);
-        }
-
-        public static bool CheckName(this AnimatorStateInfo info, ICollection<string> stateNames)
-        {
-            foreach (string str in stateNames)
-                if (info.IsName(str))
-                    return true;
-            return false;
-        }
 
         public static Transform FindRecursive(this Transform parent, string childName)
         {
