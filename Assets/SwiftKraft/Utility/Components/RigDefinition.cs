db905c9dec1acad05741ca8007060a06511d7059
SwiftKraft <shihuamian@gmail.com>
2025-12-22 12:39:07 -0500

    wip

--- Assets/SwiftKraft/Utility/Components/RigDefinition.cs
+++ Assets/SwiftKraft/Utility/Components/RigDefinition.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,65 +5,82 @@ namespace SwiftKraft.Utils
 {
     public class RigDefinition : MonoBehaviour
     {
-        public struct ReplicationData
+        public struct TransformDataNode
         {
-            public TransformData Root;
-            public TransformData[] Transforms;
+            public TransformData Transform;
+            public TransformDataNode[] Children;
 
-            public ReplicationData(RigDefinition def)
+            public TransformDataNode(TransformNode node)
             {
-                Root = new TransformData(def.Root, false);
-                Transforms = new TransformData[def.Transforms.Count];
-                for (int i = 0; i < def.Transforms.Count; i++)
-                    Transforms[i] = new TransformData(def.Transforms[i], true);
+                Transform = new(node.Transform);
+                Children = new TransformDataNode[node.Children.Length];
+
+                if (Children.Length <= 0)
+                    return;
+
+                for (int i = 0; i < Children.Length; i++)
+                    Children[i] = new(node.Children[i]);
+            }
+        }
+
+        public struct TransformNode
+        {
+            public Transform Transform;
+            public TransformNode[] Children;
+
+            public TransformNode(Transform tr)
+            {
+                Transform = tr;
+                Children = new TransformNode[Transform.childCount];
+
+                if (Children.Length <= 0)
+                    return;
+
+                for (int i = 0; i < Children.Length; i++)
+                    Children[i] = new(Transform.GetChild(i));
             }
         }
 
         public Transform Root;
+        public TransformNode RootNode { get; private set; }
 
-        public List<Transform> Transforms = new();
-
-        public Animator Animator { get; private set; }
-
-        ReplicationData replicationQueue;
+        TransformDataNode queuedReplication;
         bool queued;
-
-        private void Awake() => Animator = GetComponentInChildren<Animator>();
 
         private void LateUpdate()
         {
             if (queued)
             {
-                ReplicateInternal(replicationQueue);
                 queued = false;
+                ReplicateData(queuedReplication);
             }
         }
 
-        public void Replicate(RigDefinition source)
+        public void Replicate(RigDefinition source, bool moveUnregistered = false)
         {
             if (source == null) return;
 
-            if (source.Animator != null && source.Animator.enabled)
-            {
-                replicationQueue = new ReplicationData(source);
-                queued = true;
-                return;
-            }
-
-            ReplicateInternal(source);
+            queued = true;
+            queuedReplication = new TransformDataNode(source.RootNode);
         }
 
-        private void ReplicateInternal(RigDefinition source) => ReplicateInternal(new ReplicationData(source));
-
-        private void ReplicateInternal(ReplicationData source)
+        private void ReplicateData(TransformDataNode source)
         {
             if (Root == null)
                 return;
 
-            Root.SetPositionAndRotation(source.Root.Position, source.Root.Rotation);
-            int min = Mathf.Min(source.Transforms.Length, Transforms.Count);
-            for (int i = 0; i < min; i++)
-                Transforms[i].SetLocalPositionAndRotation(source.Transforms[i].Position, source.Transforms[i].Rotation);
+            ReplicateRecursve(RootNode, source);
+        }
+
+        private void ReplicateRecursve(TransformNode cur, TransformDataNode data)
+        {
+            cur.Transform.SetPositionAndRotation(data.Transform.Position, data.Transform.Rotation);
+
+            if (cur.Children.Length <= 0)
+                return;
+
+            for (int i = 0; i < cur.Children.Length; i++)
+                ReplicateRecursve(cur.Children[i], data.Children[i]);
         }
 
         public void Rebuild()
@@ -72,25 +88,13 @@ namespace SwiftKraft.Utils
             if (Root == null)
             {
                 Debug.LogWarning("Root is null! ");
+                RootNode = default;
                 return;
             }
 
-            Transforms.Clear();
-            RecursiveBuild(Root);
+            RootNode = new(Root);
 
-            Debug.Log($"Rebuilt rig definition from root node: \"{Root.name}\", collecting {Transforms.Count} transforms.");
-        }
-
-        private void RecursiveBuild(Transform cur)
-        {
-            if (cur != Root)
-                Transforms.Add(cur);
-
-            if (cur.childCount <= 0)
-                return;
-
-            foreach (Transform t in cur)
-                RecursiveBuild(t);
+            Debug.Log($"Rebuilt rig definition from root node: \"{Root.name}\"");
         }
     }
 }
